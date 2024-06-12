5878e6ac790e6f925245f63f72c6bc286baec231
Yuko0413 <soc13soc0422@gmail.com>
2024-06-12 17:00:55 +0900

    ログイン課題

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
+
+  get 'login', to: 'sessions#new', as: 'new_session'
+
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
+
+  get 'signup', to: 'users#new', as: 'new_user'
+
+  get 'user/:id', to: 'users#show', as: 'user'
+
+  get 'user/:id/edit', to: 'users#edit', as: 'edit_user'
+
+  #get 'sessions/new'
+  #get 'sessions/create'
+  #get 'sessions/destroy'
+
+  resources :users, only: [:create, :update, :destroy]
 end
+
